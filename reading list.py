f59668eed8725b8c388434d49ffb5f294bc7c94e
ragingbul <98214693+ragingbul@users.noreply.github.com>
2025-08-28 11:59:16 +0530

    Add reading list.py

---
+++ reading list.py
@@ -0,0 +1,5 @@
+reading_list = ["harry potter", "1984", "the fault in our statrs","the mom test","life in code"]
+reading_list.append("pachinko")
+reading_list.remove("the fault in our statrs")
+reading_list.pop(1)
+print(reading_list)
