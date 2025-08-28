f36d5d43df7ae6b2a375ec0da1f402aa2de20fda
ragingbul <98214693+ragingbul@users.noreply.github.com>
2025-08-28 11:58:46 +0530

    Add converter.py

---
+++ converter.py
@@ -0,0 +1,8 @@
+pesos = int(input('how much pesos? '))
+soles = int(input('how much soles? '))
+reais = int(input('how much reais? '))
+a = pesos / 4024.57
+b = soles / 3.52 
+c = reais / 5.45
+usd = (a + b + c)
+print ( usd )
