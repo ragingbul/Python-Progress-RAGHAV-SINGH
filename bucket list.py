0539ff6021c6afe841fb527dd6d318b857fe075b
ragingbul <98214693+ragingbul@users.noreply.github.com>
2025-08-28 11:58:44 +0530

    Add bucket list.py

---
+++ bucket list.py
@@ -0,0 +1,8 @@
+things_to_do = ['🚀 Create the dopest learn to code platform ever.','⛰️ Hike the Pacific Crest Trail.','🏡 Build an A-frame house and raise some goats.','🌏 Live somewhere in Asia for a year.','🎸 Release an album.',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip with the fam.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.']
+for i in range(len(things_to_do)):
+  print(things_to_do(i))
